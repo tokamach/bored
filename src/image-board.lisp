f82709c8e21ce6c69146bc376c035d6fe5fdaeea
Tom Hutchings <goerofmuns@gmail.com>
2018-04-04 03:32:58 +0100

    actualy sends threads over http now

--- src/image-board.lisp
+++ src/image-board.lisp
@@ -1,34 +1,54 @@
 (declaim (optimize (debug 3)))
 (defpackage :image-board
-  (:use :cl :ningle :sqlite)
+  (:use :cl :ningle :sqlite :cl-ppcre)
   (:export :*app*))
 (in-package :image-board)
 
 (defvar *app* (make-instance 'ningle:<app>))
-(defvar *db* (sqlite:connect "posts.db"))
+(defvar *db* (sqlite:connect "/Users/tom/code/lisp/image-board/posts.db"))
 
 ;; One post, if parent=nil then this is a thread
-(defstruct (post (:constructor make-post-boa (id parent title email message)))
-  id parent title email message)
+(defstruct (post (:constructor make-post-boa (id parent name message)))
+  id parent name message)
 
 ;; db interaction
-(defun create-post (id parent title email message)
-  (sqlite:execute-non-query *db*
-   "INSERT INTO Posts (id, parent, title, email, message) VALUES (?, ?, ?, ?, ?);"
-   id parent title email message))
+(defun create-post (a-post)
+  (sqlite:execute-non-query
+   *db*
+   "INSERT INTO Posts (id, parent, name, message) VALUES (?, ?, ?, ?);"
+   (post-id a-post)
+   (post-parent a-post)
+   (post-name a-post)
+   (post-message a-post)))
+
 (defun delete-post (id)
   (sqlite:execute-non-query *db*
    "DELETE FROM Posts WHERE id=?;" id))
 
-(defun get-post-from-id (id)
+(defun make-post-from-id (id)
   (let ((dbres (multiple-value-list (sqlite:execute-one-row-m-v *db* "SELECT * FROM Posts WHERE id=?;" id))))
     (apply #'make-post-boa dbres)))
 
+(defun make-post-list-from-parent (id)
+  (let ((thread-list (sqlite:execute-to-list *db* "SELECT * FROM Posts WHERE parent=? OR id=?;" id id)))
+    (mapcar #'(lambda (l) (apply #'make-post-boa l)) thread-list)))
+
 ;; creating html
-(defun generate-thread-html (id)
+(defun generate-error-html (error-string)
+  "Generate a HTML page signalling an error"
+  (format nil "<h1>Error: ~A</h1>" error-string))
+
+(defun generate-post-html-div (a-post)
+  "Generate a HTML string from a post struct"
   (concatenate 'string
-   (format nil "<h1>~D</h1>" id)
-   (format nil "<p>~A</p>" (post-message (get-post-from-id id)))))
+	       "<div>"
+	       (format nil "<h3>~D ~A<br>~A</h3>" (post-id a-post) (post-name a-post) "PLACEHOLDER DATE")
+	       (format nil "<p>~A</p>" (regex-replace-all "\\n" (post-message a-post) "<br>"))
+	       "</div>"))
+
+(defun generate-thread-html (id)
+  "Generate a HTML page from a thread id" 
+  (apply #'concatenate 'string (mapcar #'generate-post-html-div (make-post-list-from-parent id))))
 
 ;; Routes
 (setf (ningle:route *app* "/")
